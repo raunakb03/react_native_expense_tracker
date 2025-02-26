ab9fcf9b3f948282443d639fb1bf6b14b692bc87
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-02-26 12:33:03 +0530

    creating register page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -66,7 +66,7 @@ const Login = () => {
                 </View>
                 <View style={styles.footer}>
                     <Typo size={15}>Don't have an account?</Typo>
-                    <Pressable onPress={() => router.push('/(auth)/register')}>
+                    <Pressable onPress={() => router.navigate('/(auth)/register')}>
                         <Typo size={15} fontWeight={700} color={colors.primary}>Sign up</Typo>
                     </Pressable>
                 </View>
