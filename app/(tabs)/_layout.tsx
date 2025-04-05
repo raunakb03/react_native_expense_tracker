77c2e33c6d1b51a808ad474234b4811781ecb9c8
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-04-05 16:12:27 +0530

    started adding custom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,16 @@
+import CustomTabs from "@/components/CustomTabs";
 import { Tabs } from "expo-router";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
-    return <Tabs></Tabs>
+    return (
+        <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: true }}>
+            <Tabs.Screen name="index" />
+            <Tabs.Screen name="statistics" />
+            <Tabs.Screen name="wallet" />
+            <Tabs.Screen name="profile" />
+        </Tabs>
+    );
 }
 
 export default _layout;
